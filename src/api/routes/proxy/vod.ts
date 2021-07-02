3c038b97574d74b6418823409b02b9c38c32ff13
funkywoong <duswldnd12@naver.com>
2021-07-02 07:07:37 +0000

    feat:Delete express.json middleware

--- src/api/routes/proxy/vod.ts
+++ src/api/routes/proxy/vod.ts
@@ -7,8 +7,9 @@ const VOD_SERVICE_URL = "http://localhost:3001"
 export default (app) => {
 
     const filter = function (pathname, req) {
-        logger.info("API-GW proxy: ", req.headers.host);
+        logger.info("API-GW proxy: ", pathname);
         logger.info(pathname.match('^/api/vod'));
+        // console.log("req body : ", req.body)
         return pathname.match('^/api/vod');
     }
 
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -22,7 +22,7 @@ export default async ({ app }: { app: express.Application }) => {
     })
 
     // Middleware that transforms the raw string of req.body into json
-    app.use(express.json());
+    // app.use(express.json());
 
     // Load CookieParser
     app.use(cookieParser());
