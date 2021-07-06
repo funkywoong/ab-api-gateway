9636a5f4bd24301eda4720c7a1c45e86600ff101
funkywoong <duswldnd12@naver.com>
2021-07-06 09:29:41 +0000

    feat:Add reqCfUrl function

--- src/public/main.js
+++ src/public/main.js
@@ -93,3 +93,68 @@ function refreshS3List() {
         }
     });
 }
+
+function reqCfCookieUrl(assetInfo) {
+    const assetId = assetInfo.split('_')[0]
+    const profile = assetInfo.split('_')[1]
+    $.ajax({
+        url: `/api/vod/${assetId}/${profile}/cf-key`,
+        processData: false,
+        contentType: false,
+        type: 'GET',
+        success: async function(result){
+            const tgUrl = result['message']['key']
+            const signedCookie = result['message']['cookie']
+            
+            console.log(signedCookie)
+            await setCookie(signedCookie)
+            // console.log(tgUrl)
+            // window.location.href = tgUrl
+            console.log(location.origin)
+            $.ajax({
+                url: tgUrl,
+                processData: false,
+                contentType: false,
+                type: 'GET',
+                success: async function(result){
+                    console.log(result)
+                },
+                error:function(request, status, error){
+                    alert(
+                        "code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error
+                    );
+                }
+            });
+        },
+        error:function(request, status, error){
+            alert(
+                "code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error
+            );
+        }
+    });
+}
+
+async function setCookie(cookieObj) {
+    return new Promise((resolve, reject) => {
+        const CF_POLICY = "CloudFront-Policy"
+        const CF_KP_ID = "CloudFront-Key-Pair-Id"
+        const CF_SIGN = "CloudFront-Signature"
+
+        document.cookie = CF_POLICY + "=" + ''
+        document.cookie = CF_KP_ID + "=" + ''
+        document.cookie = CF_SIGN + "=" + ''
+        
+
+        // document.cookie = CF_POLICY + "=" + cookieObj[CF_POLICY] + "; domain=cdn-s3.ab-aihelper.xyz"
+        // document.cookie = CF_KP_ID + "=" + cookieObj[CF_KP_ID] + "; domain=cdn-s3.ab-aihelper.xyz"
+        // document.cookie = CF_SIGN + "=" + cookieObj[CF_SIGN] + "; domain=cdn-s3.ab-aihelper.xyz"
+
+        document.cookie = CF_POLICY + "=" + cookieObj[CF_POLICY]
+        document.cookie = CF_KP_ID + "=" + cookieObj[CF_KP_ID]
+        document.cookie = CF_SIGN + "=" + cookieObj[CF_SIGN]
+
+        console.log(document.cookie)
+
+        resolve(null)
+    })
+}
